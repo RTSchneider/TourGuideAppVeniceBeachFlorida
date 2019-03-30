ae144512338a000de500a60b2ff7df325df3b494
RTSchneider <47798726+RTSchneider@users.noreply.github.com>
2019-03-29 20:56:53 -0400

    Fragments complete. TabLayout fixed!

--- app/src/main/java/com/example/tourguideappvenicebeachflorida/MainActivity.java
+++ app/src/main/java/com/example/tourguideappvenicebeachflorida/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tourguideappvenicebeachflorida;
 
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
